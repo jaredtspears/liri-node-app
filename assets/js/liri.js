ab7698f500d63119fbe253daaf370b5f9a2d1b4a
Ha1f_Deaf <jaredtspears@gmail.com>
2018-09-30 20:27:31 -0400

    put a switch statement in... organized liri a bit more refactored some. Not working still build scaffolding

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -23,41 +23,49 @@ require('dotenv').config()
 // this config require I believe will apply to all the APIs thus placed up here:
 var datakeys = require("./assets/js/keys.js")
 var fs =require("fs");
+var liriReturn= process.argv[2];    //for node command input
+var movieName=process.argv[3]       //for node command input
+var omdb = require(keys.omdb);      
 var request = require("request");
 var inquierer = require("inquirer");
-var space = "\n" + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"; //saw another program utilize this to save space
-// ===============Spotify=======================================
+var space = "\n" 
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
-var spotify = new Spotify(
-  keys.spotify
-  // id: config.spotify.spotifyClientID,              //you must create your own spotify API id
-  // secret: config.spotify.spotifyClientSecret       // you must create your own spotify API secret
-);
- 
+// switch for commands:
+switch(liriReturn) {
+    case "concert-this":
+        concertThis();       
+        break;
+    case "spotify-this-song":
+        SpotifyThisSong();
+        break;
+    case "movie-this":
+        movieThis();       
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+    default:
+        output = "Liri found this for you: " + 
+        space + "Song Name: " + "'" + songName.toUpperCase() + "'" +
+        space + "Album Name: " + data.tracks.items[0].album.name +
+        space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +
+        space +"url: " + data.tracks.itmes[0].album.external_urls.spotify;
+        console.log(output);
+        writeToLog(output);
+}
+// ===============Spotify=======================================
+function SpotifyThisSong(){
+var params={ }; //not sure what to put in as the paramiters
 spotify.search({ type: 'track', query: songName }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  } else {
-    output = "Liri found this for you: " + 
-    space + "Song Name: " + "'" + songName.toUpperCase() + "'" +
-    space + "Album Name: " + data.tracks.items[0].album.name +
-    space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +
-    space +"url: " + data.tracks.itmes[0].album.external_urls.spotify;
-    console.log(output);
-    writeToLog(output);
-  }
- 
-console.log(data); 
-});
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  console.log(data); 
+  });
+}
 /* ====not sure if I will need this but it is here: ============
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: SpotifyClientID,
-  secret: SpotifyClientSecret 
-});
- 
 spotify
   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
   .then(function(data) {
@@ -67,30 +75,23 @@ spotify
     console.error('Error occurred: ' + err); 
   });
 */
-
-
 //===============end of Spotify ===============================
 
 // ===============BandsInTown=======================================
+function concertThis(){
 
-
-
-
-
+}
 // ===============end of BandsInTown ===============================
 
 // ===================OMDB=======================================
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
-var axios = require("axios");
-
+function movieThis(){
+var axios = require("axios")
 // Then run a request to the OMDB API with the movie specified
-axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=4cd53ff9").then(
+axios.get("http://www.omdbapi.com/?i=tt3896198&apikey="+ omdb).then(
   function(response) {
     console.log(response.data);
   }
-);
-
-
-
-
+)
+}
 // ===============end of OMDB===============================
