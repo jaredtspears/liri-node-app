b421a4da4e3de5f2e49f560d4b85a63d024726d4
Ha1f_Deaf <jaredtspears@gmail.com>
2018-10-01 22:41:15 -0400

    After class went through and added more info regarding constructor call from keys.js not tested yet but scaffolding attempting to be set.

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -1,17 +1,3 @@
-//====html front end display related JS ==============================
- // function for the anchor for possible downward page jump tab to animate down to the connect form
-//  COMMENTED OUT UNTIL ISSUE IS RESOLVED 
-
-        // $(document).ready(function){
-        //     $("a.scrollLink").click(function(event){
-        //       event.preventDefault();
-        //       $("html.body").animate({scrollTop: $($(this).
-        //         attr("href")).offset().top}, 500);
-        //     })
-        //   }
-
-//   ==========end of html front end display related JS =======================
-
 // Liri will take the following commands:
     // concert-this
     //spotifiy-this-song
@@ -21,31 +7,32 @@
 require('dotenv').config()
 
 // this config require I believe will apply to all the APIs thus placed up here:
-var datakeys = require("./assets/js/keys.js")
-var fs =require("fs");
-var liriReturn= process.argv[2];    //for node command input
-var movieName=process.argv[3]       //for node command input
-var omdb = require(keys.omdb);      
-var request = require("request");
-var inquierer = require("inquirer");
-var space = "\n" 
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
-
+var fs =require("fs");                      //currently just have this down on concertThis()
+var liriReturn= process.argv[2];            //for node command input
+var movieName=process.argv[3]               //for node command input
+var request = require("request");           //not sure where to put this "request"
+var inquierer = require("inquirer");        //not sure where to install inquirer
+var space = "\n"                            //just looks better for my styling of this code
+var keys = require("./assets/js/keys.js")   //pulling in keys.js... tried to call before the below info was requested
+var Spotify = require('node-spotify-api');  //calling spotify api 
+var spotify = new Spotify(keys.spotify);    //pulling from a constructor "spotify" on keys.js (.gitignored)
+var BIT = new BIT(keys.BIT);                //pulling from constructor "BIT" on keys.js (.gitignored)
+var OMDB = new OMDB(keys.OMDB);             //pulling from construtor "OMBD" on keys.js (.gitignored)
 // switch for commands:
 switch(liriReturn) {
-    case "concert-this":
+    case "concert-this":                
         concertThis();       
         break;
     case "spotify-this-song":
         SpotifyThisSong();
         break;
     case "movie-this":
-        movieThis();       
+        movieThis(movieName);                  //not sure if I put movie name here
         break;
     case "do-what-it-says":
         doWhatItSays();
         break;
+    // tried to prittify the default log:
     default:
         output = "Liri found this for you: " + 
         space + "Song Name: " + "'" + songName.toUpperCase() + "'" +
@@ -55,15 +42,15 @@ switch(liriReturn) {
         console.log(output);
         writeToLog(output);
 }
-// ===============Spotify=======================================
+
 function SpotifyThisSong(){
-var params={ }; //not sure what to put in as the paramiters
-spotify.search({ type: 'track', query: songName }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-  console.log(data); 
-  });
+//var params={ };           not sure if I need this
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+        return console.log('Error occurred: ' + err);
+        }
+        console.log(data); 
+    });
 }
 /* ====not sure if I will need this but it is here: ============
 spotify
@@ -75,17 +62,13 @@ spotify
     console.error('Error occurred: ' + err); 
   });
 */
-//===============end of Spotify ===============================
 
-// ===============BandsInTown=======================================
 function concertThis(){
 
+    
 }
-// ===============end of BandsInTown ===============================
-
-// ===================OMDB=======================================
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
-function movieThis(){
+function movieThis(movieName){
 var axios = require("axios")
 // Then run a request to the OMDB API with the movie specified
 axios.get("http://www.omdbapi.com/?i=tt3896198&apikey="+ omdb).then(
@@ -94,4 +77,12 @@ axios.get("http://www.omdbapi.com/?i=tt3896198&apikey="+ omdb).then(
   }
 )
 }
-// ===============end of OMDB===============================
+
+function doWhatItSays(){
+    // not sure if this fs needs to be read here
+    fs.readFile("random.txt", "utf8", function(err, /*something*/) {
+        if (err) {
+            return console.log(err);
+        }
+    })
+}
